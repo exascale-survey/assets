7deea1ed18877c6ee99b90b67a0f3fec4e3d736b
stijn <s.heldens@esciencecenter.nl>
2019-10-01 18:14:59 +0200

    Updated citation graph since some edges were missing.

--- docs/config.js
+++ docs/config.js
@@ -25,7 +25,7 @@ GexfJS.setParams({
             False for curved edges, true for straight edges
             this setting can't be changed from the User Interface
         */
-    edgeWidthFactor : 1,
+    edgeWidthFactor : 2,
         /*
             Change this parameter for wider or narrower edges
             this setting can't be changed from the User Interface
